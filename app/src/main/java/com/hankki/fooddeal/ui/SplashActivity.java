3f77f51f5d3cac95e0038c57d937ca899539a572
jochana <c01034784796@gamil.com>
2020-07-06 00:10:15 +0900

    SharedPreference 를 이용한 자동 로그인 여부 처리 로직 추가

--- app/src/main/java/com/hankki/fooddeal/ui/SplashActivity.java
+++ app/src/main/java/com/hankki/fooddeal/ui/SplashActivity.java
@@ -3,15 +3,17 @@ package com.hankki.fooddeal.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.hankki.fooddeal.R;
+import com.hankki.fooddeal.data.PreferenceManager;
 import com.hankki.fooddeal.ui.login.LoginActivity;
 
 /**스플래쉬 화면*/
 public class SplashActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +28,18 @@ public class SplashActivity extends AppCompatActivity {
         이것을 SharedPreferences에 저장한 뒤, 어플 종료시 혹은 마이페이지에서 자동 로그인 해제 시 삭제
         자동 로그인 설정 후 어플리케이션 재실행 시, 스플래시에서 SharedPreferences에 토큰이 있는지 판단해서 있으면 바로 홈 액티비티 없으면 인트로 액티비티
         */
+
+        /** SharedPreference 기본틀, key 값이나 변수타입은 추후 수정*/
+        intent = new Intent(SplashActivity.this, IntroActivity.class);
+
+        if(PreferenceManager.getBoolean(this,"autologin")){ //자동 로그인
+            intent = new Intent(SplashActivity.this, MainActivity.class);
+        }
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
                 startActivity(intent);
                 finish();
             }
