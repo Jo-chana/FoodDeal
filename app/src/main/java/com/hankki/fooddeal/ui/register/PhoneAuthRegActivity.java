9cf82b104eb13208d4afc88c1731e211e09e08ec
ggj0418 <ggj0418@naver.com>
2020-07-07 22:19:48 +0900

    회원가입 전 휴대폰 인증 액티비티 생성

---
+++ app/src/main/java/com/hankki/fooddeal/ui/register/PhoneAuthRegActivity.java
@@ -0,0 +1,16 @@
+package com.hankki.fooddeal.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hankki.fooddeal.R;
+
+public class PhoneAuthRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_auth_reg);
+    }
+}
