2f64905fd2ac3bc26c881969625e60338c682eaa
ggj0418 <ggj0418@naver.com>
2020-07-08 02:20:52 +0900

    회원가입 전 휴대폰 인증 액티비티 생성
    휴대폰 화면이 꺼지거나 잠금화면으로 가는 케이스 추가

--- app/src/main/java/com/hankki/fooddeal/ui/register/PhoneAuthRegActivity.java
+++ app/src/main/java/com/hankki/fooddeal/ui/register/PhoneAuthRegActivity.java
@@ -2,9 +2,12 @@ package com.hankki.fooddeal.ui.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.KeyguardManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +28,7 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
+// 본격 회원가입 창 이전에 휴대폰 번호를 인증하는 액티비티
 public class PhoneAuthRegActivity extends AppCompatActivity {
 
     Button authNumSendButton, authNumCheckButton;
@@ -49,6 +53,8 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
     // 자원 할당
     private void initFindViewById() {
         authNumSendButton = (Button) findViewById(R.id.auth_num_send_button);
+        // 인증번호 전송 버튼 클릭 시, 기입한 번호로 인증번호를 전송
+        // TODO DB 연동 시, 해당 휴대폰 번호 중복여부 확인
         authNumSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +62,12 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
             }
         });
         authNumCheckButton = (Button) findViewById(R.id.auth_num_check_button);
+        /*
+        인증번호 확인 버튼 클릭 시
+        1. 제한시간 내에 인증 번호를 올바르게 입력했을 경우 -> RegisterActivity로 이동
+        2. 인증 번호를 올바르게 입력했으나 제한시간을 오바한 경우 -> 재인증 요구
+        3. 인증 번호가 올바르지 않은 경우 -> 인증번호 불일치 메시지
+        */
         authNumCheckButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,6 +103,7 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
         disposable = null;
     }
 
+    // 인증번호를 포함한 문자메시지 전송
     private void setSMSTask(String phoneNo) {
         //onPreExecute
         //doInBackground
@@ -125,18 +138,20 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
                     @Override
                     public void accept(Object result) throws Exception {
                         //onPostExecute
+                        // 문자메시지를 전송한 직후에 제한시간 TimerTask를 시작
                         disposable.dispose();
                         startTimerTaks();
                     }
                 });
     }
 
+    // TimerTask 시작
     private void startTimerTaks() {
         stopTimerTask();
         timer = new Timer();
 
         timerTask = new TimerTask() {
-            int count = 300;
+            int count = 180;
 
             @Override
             public void run() {
@@ -144,7 +159,11 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         String min = Integer.toString(count/60);
-                        String sec = Integer.toString(count%60);
+                        String sec;
+
+                        if((count%60) < 10) sec = "0" + Integer.toString(count%60);
+                        else sec = Integer.toString(count%60);
+
                         String time = "0" + min + ":" + sec;
                         timerTextView.setText(time);
                     }
@@ -153,12 +172,14 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
                 if(count == 0) {
                     String authNumInput = authNumEditText.getText().toString();
                     if(!authNumInput.equals(randomAuthNum)) isAuthTimerOver = true;
+                    stopTimerTask();
                 }
             }
         };
         timer.schedule(timerTask, 0, 1000);
     }
 
+    // TimerTask 중지
     private void stopTimerTask() {
         if(timerTask != null) {
             timerTextView.setText("");
@@ -173,10 +194,11 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
         initFindViewById();
     }
 
+    // 사용자가 뒤로가기버튼으로 액티비티를 종료한 경우에서만 자원 할당 해제
     @Override
     protected void onPause() {
         super.onPause();
-        if(!isHomePressed) {
+        if(!isHomePressed && !isScreenOn() && !isDeviceLock()) {
             releaseResource();
             isHomePressed = false;
         }
@@ -187,7 +209,17 @@ public class PhoneAuthRegActivity extends AppCompatActivity {
     protected void onUserLeaveHint() {
         super.onUserLeaveHint();
         isHomePressed = true;
-        disposable = null;
+    }
 
+    // 화면꺼짐 체크
+    private boolean isScreenOn(){
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        return pm.isInteractive();
+    }
+
+    // 기기 잠금여부 체크
+    private boolean isDeviceLock(){
+        KeyguardManager myKM = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        return myKM.inKeyguardRestrictedInputMode();
     }
 }
