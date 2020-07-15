53f52cfa01382f89a7f66092a9a31f6ba0726ece
ggj0418 <ggj0418@naver.com>
2020-07-16 02:35:03 +0900

    firebase 연동 채팅구현 시작

--- app/src/main/java/com/hankki/fooddeal/ui/chatting/ChattingFragment.java
+++ app/src/main/java/com/hankki/fooddeal/ui/chatting/ChattingFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,14 +14,22 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hankki.fooddeal.R;
 import com.hankki.fooddeal.data.staticdata.StaticChatRoom;
+import com.hankki.fooddeal.ux.firebasehelper.FirestoreAdapter;
 import com.hankki.fooddeal.ux.itemtouchhelper.ItemTouchHelperCallback;
 import com.hankki.fooddeal.ux.itemtouchhelper.ItemTouchHelperExtension;
 import com.hankki.fooddeal.ux.itemtouchhelper.MainRecyclerAdapter;
 import com.hankki.fooddeal.ux.itemtouchhelper.ChatRoomItem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 /**채팅 화면*/
 public class ChattingFragment extends Fragment {
@@ -32,12 +42,31 @@ public class ChattingFragment extends Fragment {
 
     Context mContext;
 
+    //TODO firebase 연동해야하는데 왜 안될까.........
+    /*이현준*/
+    Button setChatButton;
+    private FirestoreAdapter firestoreAdapter;
+    private WeakHashMap<String, String> selectedUsers = new WeakHashMap<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.fragment_chatting, container, false);
         mContext = getContext();
+
+        /*이현준*/
+        setChatButton = view.findViewById(R.id.setChat);
+        setChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO 게스트 자신의 UID 입력
+                selectedUsers.put("ggj0418", "");
+
+                DocumentReference newRoom = FirebaseFirestore.getInstance().collection("rooms").document();
+                CreateChattingRoom(newRoom);
+            }
+        });
+
 
         setRecyclerView(view);
         return view;
@@ -61,6 +90,36 @@ public class ChattingFragment extends Fragment {
         chatRoomItems = staticChatRoom.getChatItems();
         return chatRoomItems;
     }
+
+
+    public void CreateChattingRoom(final DocumentReference room) {
+        // TODO 게시글 정보에서 글 작성자의 아이디 혹은 UID를 가져와서 uid에 저장
+        String uid = "dlguwn13";
+        Map<String, Integer> users = new HashMap<>();
+        StringBuilder titleStringBuilder = new StringBuilder();
+        String title = "";
+        for( String key : selectedUsers.keySet()) {
+            users.put(key, 0);
+            if (title.length() < 20 & !key.equals(uid)) {
+                titleStringBuilder.append(selectedUsers.get(key));
+                titleStringBuilder.append(", ");
+                title = titleStringBuilder.toString();
+            }
+        }
+        Map<String, Object> data = new HashMap<>();
+        data.put("title", title.substring(0, title.length() - 2));
+        data.put("users", users);
+
+        room.set(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getContext(), "성공!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 
     @Override
     public void onResume() {
