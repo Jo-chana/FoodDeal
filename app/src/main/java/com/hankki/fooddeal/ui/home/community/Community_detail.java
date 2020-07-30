3935eeeab0769291bfa6735b3542685ca5539925
Lee-Soyeon <leesoyena@gmail.com>
2020-07-31 01:26:27 +0900

    게시글 상세 화면에서 지도 확대/축소 동작 사용 중지 기능 추가

--- app/src/main/java/com/hankki/fooddeal/ui/home/community/Community_detail.java
+++ app/src/main/java/com/hankki/fooddeal/ui/home/community/Community_detail.java
@@ -39,6 +39,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
@@ -567,6 +568,7 @@ public class Community_detail extends AppCompatActivity implements OnMapReadyCal
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latlng);
+
         markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_icon_home));
         markerOptions.anchor((float)0.5,(float)0.5);
 
@@ -581,6 +583,9 @@ public class Community_detail extends AppCompatActivity implements OnMapReadyCal
 
         cameraUpdate = CameraUpdateFactory.newLatLng(latlng);
         mapPost.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 17));
+
+        UiSettings uiSettings = googleMap.getUiSettings();
+        uiSettings.setZoomGesturesEnabled(false);
     }
 
     @Override
